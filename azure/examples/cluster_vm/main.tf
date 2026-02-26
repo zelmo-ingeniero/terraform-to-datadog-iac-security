533436ab5f62a131f2d6d8f2ff69b128b5189fac
zelmo-ingeniero <vsergiojared@gmail.com>
2026-02-26 10:57:50 -0600

    removing storage

--- azure/examples/cluster_vm/main.tf
+++ azure/examples/cluster_vm/main.tf
@@ -115,16 +115,6 @@ resource "azurerm_linux_virtual_machine" "test" {
   admin_username = var.username
 }
 
-resource "azurerm_managed_disk" "test" {
-  count                = 2
-  name                 = "datadisk_existing_${count.index}"
-  location             = azurerm_resource_group.rg.location
-  resource_group_name  = azurerm_resource_group.rg.name
-  storage_account_type = "Standard_LRS"
-  create_option        = "Empty"
-  disk_size_gb         = "1024"
-}
-
 resource "azurerm_virtual_machine_data_disk_attachment" "test" {
   count              = 2
   managed_disk_id    = azurerm_managed_disk.test[count.index].id
